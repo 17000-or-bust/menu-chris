33d25493694c966cc8af5bc5ff359a5bd50b1f7c
Chris Tso <christophertso2017@u.northwestern.edu>
2019-03-07 19:42:41 -0800

    Edited server code

--- server2.js
+++ server2.js
@@ -2,19 +2,16 @@ var nr = require('newrelic');
 var connection = require('./database/newIndex.js');
 var express = require("express");
 var app = express();
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 // const connection = require("./server/db/");    
 
 var PORT = 3000;
 
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(express.static('./client/dist/'));
 app.listen(PORT);
 
-app.get('/api/menu/:meal/:id', (req, res) =>{
-    // var type = req.params.meal;
-    // var id = req.params.id;   
-    // let queryStr = `select * from ${req.params.meal} WHERE restaurant_id = ${req.params.id}`;
+app.get('/api/menu/:meal/:id', (req, res) => {
     connection.query(`select * from ${req.params.meal} WHERE restaurant_id = ${req.params.id}`, function (err, result) {
       if (err) {
         console.log("error message: ", err);
@@ -24,11 +21,9 @@ app.get('/api/menu/:meal/:id', (req, res) =>{
     });
 })
 
-app.post('/api/menu/:meal', (req, res) =>{
+app.post('/api/menu/:meal', (req, res) => {
   var type = req.params.meal;
-  // console.log('BODY', req.body, type);
-  // var query = `INSERT INTO ${type} (restaurant_id, name, description, price) VALUES (${req.body.restaurant_id}, '${req.body.name}', '${req.body.description}', ${req.body.price})`;  
-  var query = `INSERT INTO ${type} (restaurant_id, name, description, price) VALUES (10000001, 'chris', 'A pizza', 19)`;  
+  var query = `INSERT INTO ${type} (restaurant_id, name, description, price) VALUES (${req.body.restaurant_id}, '${req.body.name}', '${req.body.description}', ${req.body.price})`;  
   connection.query(query, function (err, result) {
     if (err) {
       console.log("error message: ", err);
